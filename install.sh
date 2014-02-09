e6b9fefbbb9ab5a0412a5fb2aabecddb3df90301
Mattias Erming <mattias@mattiaserming.com>
2014-02-09 15:03:58 -0800

    Download and unzip Wordpress

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-pkgs=(
-	"apache2"
-	"libapache2-mod-php5"
-	"mysql-server"
-	"php5-gd"
-	"php5-mysql"
-)
+pkgs="
+apache2
+libapache2-mod-php5
+mysql-server
+php5-gd
+php5-mysql
+unzip
+wget
+"
 
-for pkg in ${pkgs[@]}
-do
-	sudo apt-get install -y $pkg
-done
+sudo apt-get install -y $pkgs
 
 sudo a2enmod rewrite
 sudo service apache2 restart
+
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+
+cd /var/www
+sudo wget http://wordpress.org/latest.zip
+sudo unzip latest.zip
+sudo mv wordpress/* .
+sudo rm -rf index.html wordpress latest.zip
+
+sudo chmod 775 /var/www
+sudo chown www-data:www-data /var/www
+sudo adduser $USER www-data
+
+echo
+echo "Script complete!"
+echo "Please relog current user before proceeding."
