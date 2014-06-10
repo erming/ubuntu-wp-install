bbbb2c2ceb3c8e29a8a61d33564d908eff97e1e5
Mattias Erming <mattias@mattiaserming.com>
2014-06-10 07:42:18 -0700

    Use chmod

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ if [ ! -d "$dir" ]; then
 	sudo mkdir -p $dir
 fi
 
-sudo apt-get install -y mysql-server
-sudo apt-get install -y apache2
-sudo apt-get install -y libapache2-mod-php5
-sudo apt-get install -y php5-gd
-sudo apt-get install -y php5-mysql
-sudo apt-get install -y wget
-sudo apt-get install -y unzip
+sudo apt-get install -y \
+	mysql-server \
+	apache2 \
+	libapache2-mod-php5 \
+	php5-gd \
+	php5-mysql \
+	wget \
+	unzip
 
 sudo a2enmod rewrite
 sudo replace "2M" "10M" -- /etc/php5/apache2/php.ini
@@ -33,15 +34,12 @@ sudo mv wordpress/* .
 sudo rm -rf index.html wordpress latest.zip
 
 sudo chown -R www-data:www-data $dir
+sudo chmod -R 775 $dir
 
 if !(groups $USER | grep >/dev/null www-data); then
 	sudo adduser $USER www-data
 	sudo newgrp www-data
 fi
-
-find $dir -type d | while read dir
-	do sudo chmod 775 $dir
-done
 
 cd - >/dev/null
 echo "Script complete!"
